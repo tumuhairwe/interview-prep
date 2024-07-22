19e2f2d92901b821fa6204d1fae5c3d8c1c24234
Judes Tumuhairwe <tumuhairwe@gmail.com>
2024-07-21 23:15:57 -0400

    Added better impl fo EditDistance

--- src/com/tumuhairwe/prep/dynamicprogramming/EditDistance.java
+++ src/com/tumuhairwe/prep/dynamicprogramming/EditDistance.java
@@ -14,69 +14,33 @@ public class EditDistance {
     public static void main(String[] args) {
         System.out.println(minDistance("horse", "ros"));
     }
-    // ref:https://leetcode.com/problems/edit-distance/solutions/5401748/java-solution-easy-solution-easy-he-bhai-step-follow-karloo/?envType=problem-list-v2&envId=plakya4j&
     public static int minDistance(String word1, String word2) {
-        // base case: if either 1 is empty ... return length of the other
-        if(word1.length() == 0){
-            return word2.length();
-        }
-        else if(word2.length() == 0){
-            return word1.length();
-        }
+        return minDistanceRecursive(word1, word2, word1.length(), word2.length());
+    }
 
-        //1. init 2D array to store cost of converting substring from word1 to word2
-        int[][] costMatrix = new int[word1.length()][word2.length()];
-
-        //3. determine the starting point for filling the costMatrix
-        int lastRow = word1.length() - 1;
-        int lastCol = word2.length() - 1;
-        boolean isMatch = true;
-        if(word1.charAt(lastRow) != word2.charAt(lastCol)){
-            costMatrix[lastRow][lastCol] = 1;
-            isMatch = false;
+    private static int minDistanceRecursive(String word1, String word2, int word1Index, int word2Index) {
+        //0. base case: if we've reached the end of either, return the length of the other
+        if(word1Index == 0){
+            return word2Index;
+        }
+        else if(word2Index == 0){
+            return word1Index;
         }
 
-        //4a) fill lastCol based on whether the chars match or not
-        // iterate lastCol backwards
-        for (int j = lastCol - 1; j >= 0; j--) {
-            if(word1.charAt(lastRow) == word2.charAt(j)){
-                isMatch = true;
-            }
-
-            costMatrix[lastRow][j] = lastCol - j + 1;
-            if(isMatch){
-                costMatrix[lastRow][j]--;   // set value to 0 (i.e. isMatch -> nothing to be done)
-            }
+        //1. if the chars are the same, recurse-with index - 1
+        if(word1.charAt(word1Index - 1) == word2.charAt(word2Index - 1)){
+            return minDistanceRecursive(word1, word2, word1Index - 1, word2Index - 1);
         }
+        else{
+            //2. calculate the min of insert/delete and replace
+            int insertOperationCost = minDistanceRecursive(word1, word2, word1Index, word2Index - 1);
+            int deleteOperationCost = minDistanceRecursive(word1, word2, word1Index - 1, word2Index);
+            int replaceOperationCost = minDistanceRecursive(word1, word2, word1Index - 1, word2Index - 1);
 
-        //4b fill the lastRow based on whether the chars match
-        isMatch = word1.charAt(lastRow) == word2.charAt(lastCol);
-        for (int i = lastRow - 1; i >= 0; i--) {
-            if(word1.charAt(i) == word2.charAt(lastCol)){
-                isMatch = true;
-            }
+            int minOfInsertOrDelete = Math.min(insertOperationCost, deleteOperationCost);
 
-            costMatrix[i][lastCol] = lastRow - i + 1;
-            if(isMatch){
-                costMatrix[i][lastCol]--;
-            }
+            //return the lesser of the 3 operations
+            return Math.min(minOfInsertOrDelete, replaceOperationCost) + 1;
         }
-
-        //5. fill remainder of 2D array going backwards
-        //  a) if chars match, take the diagonal value (costMattrix[i+1][j+1])
-        //  b) if chars don't match, take the minimum of the 3 possible operations (insert, delete or replace) and add 1;
-        for (int i = lastRow - 1; i >= 0; i--) {
-            for (int j = lastCol - 1; j >= 0; j--) {
-                if(word1.charAt(i) == word2.charAt(j)){
-                    costMatrix[i][j] = costMatrix[i+1][j+1];
-                }
-                else{
-                    int min = Math.min(costMatrix[i + 1][j], costMatrix[i][j + 1]);
-                    min = Math.min(min, costMatrix[i + 1][j + 1]);
-                    costMatrix[i][j] = 1 + min;   // min = costOfChange + 1
-                }
-            }
-        }
-        return costMatrix[0][0];    //top-left gives the minumum number of operations to convert word1 to word1 (e.g. horse -> ros)
     }
 }
