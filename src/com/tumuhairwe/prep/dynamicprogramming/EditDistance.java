122971cef42ab7e7b1589ea946452bbc552a04a6
Judes Tumuhairwe <tumuhairwe@gmail.com>
2024-07-21 23:03:45 -0400

    Fixed edit distance impl

--- src/com/tumuhairwe/prep/dynamicprogramming/EditDistance.java
+++ src/com/tumuhairwe/prep/dynamicprogramming/EditDistance.java
@@ -14,8 +14,9 @@ public class EditDistance {
     public static void main(String[] args) {
         System.out.println(minDistance("horse", "ros"));
     }
-    public static int minDistance(String word1, String word2){
-        //0. base case: if either is empty, ... return length of the others
+    // ref:https://leetcode.com/problems/edit-distance/solutions/5401748/java-solution-easy-solution-easy-he-bhai-step-follow-karloo/?envType=problem-list-v2&envId=plakya4j&
+    public static int minDistance(String word1, String word2) {
+        // base case: if either 1 is empty ... return length of the other
         if(word1.length() == 0){
             return word2.length();
         }
@@ -26,7 +27,7 @@ public class EditDistance {
         //1. init 2D array to store cost of converting substring from word1 to word2
         int[][] costMatrix = new int[word1.length()][word2.length()];
 
-        //2. determine the starting point for filling the costMatrix
+        //3. determine the starting point for filling the costMatrix
         int lastRow = word1.length() - 1;
         int lastCol = word2.length() - 1;
         boolean isMatch = true;
@@ -35,9 +36,9 @@ public class EditDistance {
             isMatch = false;
         }
 
-        //4a: fill lastCost based on whether the chars match or not
-        // iterat lastCost backwards
-        for (int j = lastCol - 1; j >=0; j--) {
+        //4a) fill lastCol based on whether the chars match or not
+        // iterate lastCol backwards
+        for (int j = lastCol - 1; j >= 0; j--) {
             if(word1.charAt(lastRow) == word2.charAt(j)){
                 isMatch = true;
             }
@@ -48,21 +49,34 @@ public class EditDistance {
             }
         }
 
-        //5. fill the remainder of 2D array going backwards
-        // a) if chars match, take the diagonal value (costMatrix[i+1][j+1]
-        // b) if chas don't match, take the minimum of the 3 possible operaions (insert, delete or replace) and add 1;
+        //4b fill the lastRow based on whether the chars match
+        isMatch = word1.charAt(lastRow) == word2.charAt(lastCol);
         for (int i = lastRow - 1; i >= 0; i--) {
-            for (int j = lastCol - 1; j >= 0 ; j--) {
-                if(word1.charAt(lastRow) == word2.charAt(lastCol)){
+            if(word1.charAt(i) == word2.charAt(lastCol)){
+                isMatch = true;
+            }
+
+            costMatrix[i][lastCol] = lastRow - i + 1;
+            if(isMatch){
+                costMatrix[i][lastCol]--;
+            }
+        }
+
+        //5. fill remainder of 2D array going backwards
+        //  a) if chars match, take the diagonal value (costMattrix[i+1][j+1])
+        //  b) if chars don't match, take the minimum of the 3 possible operations (insert, delete or replace) and add 1;
+        for (int i = lastRow - 1; i >= 0; i--) {
+            for (int j = lastCol - 1; j >= 0; j--) {
+                if(word1.charAt(i) == word2.charAt(j)){
                     costMatrix[i][j] = costMatrix[i+1][j+1];
                 }
-                else {
+                else{
                     int min = Math.min(costMatrix[i + 1][j], costMatrix[i][j + 1]);
                     min = Math.min(min, costMatrix[i + 1][j + 1]);
                     costMatrix[i][j] = 1 + min;   // min = costOfChange + 1
                 }
             }
         }
-        return costMatrix[0][0]; //top-left gives the minumum number of operations to convert word1 to word1 (e.g. horse -> ros)
+        return costMatrix[0][0];    //top-left gives the minumum number of operations to convert word1 to word1 (e.g. horse -> ros)
     }
 }
